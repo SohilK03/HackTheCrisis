eff478843ad904a21ffc94125e0d020fe9a3e610
Sohil Khattar <sohilkhattar123@gmail.com>
2021-05-12 11:44:02 +0530

    Video BG

--- src/components/Hero/Hero.component.jsx
+++ src/components/Hero/Hero.component.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import Fade from "react-reveal/Fade";
 
-import video from '../../assets/video/teaser.mp4';
-import poster from '../../assets/encore-logo/logo-black.png';
+import video from "../../assets/video/teaser.mp4";
+import poster from "../../assets/encore-logo/logo-black-2.svg";
 
-import './Hero.styles.scss';
+import "./Hero.styles.scss";
 
 const Hero = () => {
   return (
-    <section id='home' className='section section-hero'>
+    <section id="home" className="section section-hero">
       <Fade>
-        <div className='logo-container'>
+        <div className="logo-container">
           <video src={video} poster={poster} autoPlay loop muted playsInline />
         </div>
       </Fade>
