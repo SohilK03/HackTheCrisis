9025618919b71c1802c0ceb4cdaa9debc22aaf24
Sohil Khattar <sohilkhattar123@gmail.com>
2021-05-13 13:41:24 +0530

    Meet the team fix

--- src/components/Team/Team.component.jsx
+++ src/components/Team/Team.component.jsx
@@ -1,29 +1,37 @@
-import React, { useEffect } from 'react';
-import Fade from 'react-reveal/Fade';
-import { animateScroll as scroll } from 'react-scroll';
+import React, { useEffect } from "react";
+import Fade from "react-reveal/Fade";
+import { animateScroll as scroll } from "react-scroll";
 
-import Person from '../Person/Person.component';
+import Person from "../Person/Person.component";
 
-import { TeamData } from '../../data/team.data';
-
+import { TeamData } from "../../data/team.data";
+import bg from "../../assets/team/Asset 1.svg";
 // import { ReactComponent as Illustration } from '../../assets/illustrations/team.svg';
 // import placeholder from "../../assets/about-section/placeholder.svg"
-import placeholder_2 from "../../assets/about-section/placeholder_2.svg"
-import './Team.styles.scss';
+import placeholder_2 from "../../assets/about-section/placeholder_2.svg";
+import "./Team.styles.scss";
 
 const Team = () => {
   useEffect(() => {
     scroll.scrollToTop();
   }, []);
-  
+
   return (
-    <section id='team' className='section section-team'>
-      <img className='placeholder' alt={'pb'} src={placeholder_2}/>
-    <img className='placeholder_2' alt={'pb2'} src={placeholder_2}/>
+    <section
+      id="team"
+      className="section section-team"
+      style={{
+        background: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "right bottom",
+      }}
+    >
+      {/* <img className='placeholder' alt={'pb'} src={placeholder_2}/> */}
+      <img className="placeholder_2" alt={"pb2"} src={placeholder_2} />
       <Fade bottom>
-        <h1 className='section-heading'>Meet The Team</h1>
-        <div className='section-content'>
-          <div className='members'>
+        <h1 className="section-heading">Meet The Team</h1>
+        <div className="section-content">
+          <div className="members">
             <Fade bottom delay={150}>
               {TeamData.members.map((member) => (
                 <Person key={member.key} member={member} />
