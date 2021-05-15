6c8bdd4bc1a70d1d4538b0f4bc7f39ae450998cc
aavaigrocks2001 <aavaigmalhotra2001@gmail.com>
2021-05-15 18:44:59 +0530

    updated links

--- src/data/events.data.js
+++ src/data/events.data.js
@@ -5,16 +5,16 @@ import React from 'react';
 export const eventsList = [
   {
     code: 'hack-1',
-    name: 'Sci-Pher Hunt'
+    name: 'Sci-Pher Hunt',
   },
   {
     code: 'hack-2',
-      name: 'Mental Health Talks'
+    name: 'Mental Health Talks',
   },
   {
     code: 'hack-3',
-      name: 'Covid-Relief'
-  }
+    name: 'Covid-Relief',
+  },
 ];
 
 export const events = {
@@ -25,17 +25,21 @@ export const events = {
       date: '20th May, 2021',
       team: '2-4 Members',
       //  prize: <>{Rs()} Please change this</>,
-      registrationFee : 'FREE',
-     
-      registrationLink:
-        'https://forms.gle/HXwBHKYTyMDiiPVS7',
+      registrationFee: 'FREE',
+
+      registrationLink: 'https://dare2compete.com/o/wBWjnfY?refId=SkKQCY0',
       poster: require('../assets/events/ScipherHunt-left.png'),
       description: (
         <p>
-         In a test of your sharpness, this quirky yet interesting challenge will test your awareness and get you to think on your feet. You will go through a series of decoding and answering questions at different levels, before moving on to the next stage. You don’t need any prior subject knowledge for this event, just make sure you brush up on those problem solving skills! So buckle up for this brain tickling rollercoaster ride.
-
+          In a test of your sharpness, this quirky yet interesting challenge
+          will test your awareness and get you to think on your feet. You will
+          go through a series of decoding and answering questions at different
+          levels, before moving on to the next stage. You don’t need any prior
+          subject knowledge for this event, just make sure you brush up on those
+          problem solving skills! So buckle up for this brain tickling
+          rollercoaster ride.
         </p>
-      )
+      ),
     },
     {
       code: 'hack-2',
@@ -43,20 +47,22 @@ export const events = {
       date: '26th May, 2019',
       team: '1 member',
       //prize: <>{Rs()} Please change this</>,
-      registrationFee : 'FREE',
+      registrationFee: 'FREE',
 
       registrationLink:
         'https://docs.google.com/forms/d/e/1FAIpQLSflsZYw3nu1SyOMAPGnezoD3wy2mPUMhyESiLSNxoZAwnFJzQ/viewform?usp=sf_link',
       poster: require('../assets/events/Mental Health Talks-mid.png'),
       description: (
         <p>
-          Quarantine, in particular, and its impact on many people&#39;s normal activities, habits, or livelihood
-are forcing them into a bleak world of isolation, depression, unhealthy alcohol and drug use, and
-learning to cope with the stress and anxiety it has caused; ISTE is hosting a discussion on
-mental health under the direction of Dr. anonymous on this subject in order to help you become
-mentally fit and healthy, allowing you to handle emotionally stressful circumstances with ease.
+          Quarantine, in particular, and its impact on many people&#39;s normal
+          activities, habits, or livelihood are forcing them into a bleak world
+          of isolation, depression, unhealthy alcohol and drug use, and learning
+          to cope with the stress and anxiety it has caused; ISTE is hosting a
+          discussion on mental health under the direction of Dr. anonymous on
+          this subject in order to help you become mentally fit and healthy,
+          allowing you to handle emotionally stressful circumstances with ease.
         </p>
-      )
+      ),
     },
     {
       code: 'hack-3',
@@ -64,17 +70,23 @@ mentally fit and healthy, allowing you to handle emotionally stressful circumsta
       date: 'On Going!',
       team: '1 member',
       //prize: <>{Rs()} Please change this</>,
-      registrationFee : 'FREE',
-      
-      registrationLink:
-        'https://twitter.com/istecovidrelief',
+      registrationFee: 'FREE',
+
+      registrationLink: 'https://twitter.com/istecovidrelief',
       poster: require('../assets/events/covid.png'),
       description: (
         <p>
-          As the second wave of COVID-19 continues to scourge our country, hospitals across India are facing a dire shortage of beds and oxygen cylinders. The safety of people across the country and abroad is a matter of great priority. As young budding engineers and citizens of the country, we have a strong urge to help those in need to our fullest potential. Collaborating with various ISTE chapters throughout the country, we have started a Covid Relief drive to help people in need with the help of social media platforms. We humbly request you to donate, and your little bit for the country.
+          As the second wave of COVID-19 continues to scourge our country,
+          hospitals across India are facing a dire shortage of beds and oxygen
+          cylinders. The safety of people across the country and abroad is a
+          matter of great priority. As young budding engineers and citizens of
+          the country, we have a strong urge to help those in need to our
+          fullest potential. Collaborating with various ISTE chapters throughout
+          the country, we have started a Covid Relief drive to help people in
+          need with the help of social media platforms. We humbly request you to
+          donate, and your little bit for the country.
         </p>
-      )
-    }
-  
-  ]
+      ),
+    },
+  ],
 };
--- src/pages/Events/HackTheCrisis.jsx
+++ src/pages/Events/HackTheCrisis.jsx
@@ -1,186 +1,199 @@
-import React,{useState} from 'react';
-import { Container, Jumbotron, Row} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Jumbotron, Row } from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDoubleDown ,faLink} from '@fortawesome/free-solid-svg-icons';
+import { faAngleDoubleDown, faLink } from '@fortawesome/free-solid-svg-icons';
 import Countdown from 'react-countdown';
 
+export const HackTheCrisis = () => {
+  const listOfSections = [
+    {
+      name: 'About',
+      id: 'about',
+      render: (
+        <Fade>
+          <h2 className='section-sub-heading' id='about'>
+            <FontAwesomeIcon
+              icon={faAngleDoubleDown}
+              className='icon arrow-down'
+            />
+            About
+            <FontAwesomeIcon
+              icon={faAngleDoubleDown}
+              className='icon arrow-down'
+            />
+          </h2>
+          <p>
+            The hackathon - &quot;HacktheCrisis&quot; - is being organized in
+            collaboration with the students&#39; chapter of Team ISTE from
+            various colleges across the nation to solicit suggestions for
+            containing the spread of coronavirus and developing strategies to
+            cope with its aftermath.
+          </p>
+        </Fade>
+      ),
+    },
+    {
+      name: 'Problem Statements',
+      id: 'problem-statements',
+      render: (
+        <Fade>
+          <h2 className='section-sub-heading' id='problem-statements'>
+            <FontAwesomeIcon
+              icon={faAngleDoubleDown}
+              className='icon arrow-down'
+            />
+            Problem Statements
+            <FontAwesomeIcon
+              icon={faAngleDoubleDown}
+              className='icon arrow-down'
+            />
+          </h2>
+          <p>
+            The participating teams will be working to formulate solutions for
+            problem statements similar to the ones mentioned below.
+          </p>
+          <ol>
+            <li>
+              1. Aiding local businesses in current and post-pandemic
+              situations.
+            </li>
+            <li>2. Administering care to those who are at immediate risk.</li>
+            <li>
+              3. Downsizing the development and use of duplicate medicines.
+            </li>
+          </ol>
+        </Fade>
+      ),
+    },
+    {
+      name: 'Registration Link',
+      id: 'registration-link',
+      render: (
+        <Fade>
+          <h2 className='section-sub-heading' id='registration-link'>
+            <FontAwesomeIcon icon={faLink} />
+            Registration Link
+          </h2>
+          <p>
+            Our contest will be hosted on D2C. Please{' '}
+            <a
+              style={{ color: 'blue' }}
+              href={'https://dare2compete.com/o/pySvm2u?refId=SkKQCY0'}
+              target='__blank'
+            >
+              click here
+            </a>{' '}
+            to register
+          </p>
+        </Fade>
+      ),
+    },
+  ];
 
-export const HackTheCrisis = ()=>{
+  const [isListOpen, toggleList] = useState(false);
+  const [selectedSection, toggleSelection] = useState(listOfSections[0]);
 
-    const listOfSections=[
-        {
-            name:'About',
-            id:'about',
-            render: 
-            <Fade>
-                <h2 className='section-sub-heading' id="about">
-                    <FontAwesomeIcon
-                        icon={faAngleDoubleDown}
-                        className='icon arrow-down'
-                    />
-                    About
-                    <FontAwesomeIcon
-                        icon={faAngleDoubleDown}
-                        className='icon arrow-down'
-                    />
-                </h2>
-                <p>
-                    The hackathon - &quot;HacktheCrisis&quot; - is being organized in collaboration with the students&#39; chapter of Team ISTE from various colleges across the nation to solicit suggestions for containing the spread of coronavirus and developing strategies to cope with its aftermath.
-                    
-                </p>
-            </Fade>
-
-        },
-        {
-            name:'Problem Statements',
-            id:'problem-statements',
-            render:
-            <Fade>
-                <h2 className='section-sub-heading' id="problem-statements">
-                    <FontAwesomeIcon
-                        icon={faAngleDoubleDown}
-                        className='icon arrow-down'
-                    />
-                    Problem Statements
-                    <FontAwesomeIcon
-                        icon={faAngleDoubleDown}
-                        className='icon arrow-down'
-                    />
-                </h2>
-                <p>
-                The participating teams will be working to formulate solutions for problem statements similar to the ones mentioned below.
-                </p>
-                <ol>
-                    <li>1. Aiding local businesses in current and post-pandemic situations.</li>
-                    <li>2. Administering care to those who are at immediate risk.</li>
-                    <li>3. Downsizing the development and use of duplicate medicines.</li>
-                </ol>
-            </Fade>
-        },
-        {
-            name:'Registration Link',
-            id:'registration-link',
-            render:
-            <Fade>
-                <h2 className='section-sub-heading' id="registration-link">
-                    <FontAwesomeIcon
-                        icon={faLink}
-                    
-                    /> 
-                    Registration Link
-                </h2>
-                <p>Our contest will be hosted on D2C. Please <a style={{color:'blue'}} href={'https://google.com'} target="__blank">click here</a> to register</p>
-            </Fade>
-        },
-
-    ]
-
-
-    const [isListOpen, toggleList] = useState(false);
-    const [selectedSection, toggleSelection] = useState(listOfSections[0]);
-
-    const searchBar=()=>{
-    return(
-      <Fade bottom delay={150}>
-      <div
-        className={`events-list ${isListOpen ? 'open' : ''} mt-4`}
-        onClick={() => toggleList(!isListOpen)}
-      >
-        <div className='title'>{selectedSection.name}</div>
-        <div className='list'>
-          <ul>
-              {listOfSections.map((section,index)=>{
-                  return(
-                    <li key={index}>
-                        <span style={{cursor:'pointer'}} onClick={()=>toggleSelection(listOfSections[index])} offset='620'>
-                            {section.name}
-                        </span>
-                    </li>
-                  );
-
-              })}
-            
-          </ul>
-        </div>
-      </div>
-    </Fade>
-    );
-  }
-
-  const renderer= ({hours,minutes,seconds,days}) =>{
-    
+  const searchBar = () => {
     return (
-        <Container>
-            <Row>
-                <div className="col-3 border-right">
-                    <span className="fs-1">{days}</span>
-                    <br/>
-                    <span className="text-muted fs-6">Days</span>
-                </div>
-                <div className="col-3 border-right">
-                    <span className="fs-1">{hours}</span>
-                    <br/>
-                    <span className="text-muted fs-6">Hours</span>
-                </div>
-                <div className="col-3 border-right">
-                    <span className="fs-1">{minutes}</span>
-                    <br/>
-                    <span className="text-muted fs-6">Mins</span>
-                    
-                </div>
-                <div className="col-3">
-                    <span className="fs-1">{seconds}</span>
-                    <br/>
-                    <span className="text-muted fs-6">Secs</span>
-                </div>
-                
-            </Row>
-        </Container>
+      <Fade bottom delay={150}>
+        <div
+          className={`events-list ${isListOpen ? 'open' : ''} mt-4`}
+          onClick={() => toggleList(!isListOpen)}
+        >
+          <div className='title'>{selectedSection.name}</div>
+          <div className='list'>
+            <ul>
+              {listOfSections.map((section, index) => {
+                return (
+                  <li key={index}>
+                    <span
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => toggleSelection(listOfSections[index])}
+                      offset='620'
+                    >
+                      {section.name}
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </Fade>
     );
+  };
 
-  }
+  const renderer = ({ hours, minutes, seconds, days }) => {
+    return (
+      <Container>
+        <Row>
+          <div className='col-3 border-right'>
+            <span className='fs-1'>{days}</span>
+            <br />
+            <span className='text-muted fs-6'>Days</span>
+          </div>
+          <div className='col-3 border-right'>
+            <span className='fs-1'>{hours}</span>
+            <br />
+            <span className='text-muted fs-6'>Hours</span>
+          </div>
+          <div className='col-3 border-right'>
+            <span className='fs-1'>{minutes}</span>
+            <br />
+            <span className='text-muted fs-6'>Mins</span>
+          </div>
+          <div className='col-3'>
+            <span className='fs-1'>{seconds}</span>
+            <br />
+            <span className='text-muted fs-6'>Secs</span>
+          </div>
+        </Row>
+      </Container>
+    );
+  };
 
-  const renderCountDown = ()=>{
-      console.log(Date.UTC(2021,5,20));
-      return (
+  const renderCountDown = () => {
+    console.log(Date.UTC(2021, 5, 20));
+    return (
       <>
-        <h2 className='section-sub-heading' id="problem-statements">
-           
-            Starts in...
-            
+        <h2 className='section-sub-heading' id='problem-statements'>
+          Starts in...
         </h2>
-        <Countdown date={'2021-05-20T00:00:00'} renderer={renderer}/>
-    </>
-        
-      );
-  }
+        <Countdown date={'2021-05-20T00:00:00'} renderer={renderer} />
+      </>
+    );
+  };
 
+  return (
+    <section id='events' className='section section-events'>
+      <Fade top>
+        <Jumbotron
+          className='mt-3'
+          style={{
+            backgroundColor: '#212121',
+            color: '#fff',
+            textAlign: 'center',
+            marginTop: '50px',
+            padding: '10px',
+          }}
+        >
+          <h1>HackTheCrisis</h1>
+          <p>
+            Any new crisis necessitates a fresh collection of ideas and methods
+            for coping with it. As a result, it is up to us to pool our
+            resources and devise ideas and strategies that can be quickly
+            developed using technology and deployed on the ground to counter the
+            outbreak of the COVID-19 pandemic
+          </p>
+        </Jumbotron>
+      </Fade>
 
-
-
-
-    return(
-     
-        <section id='events' className='section section-events' >
-            <Fade top>
-                <Jumbotron className="mt-3" style={{backgroundColor:'#212121', color:'#fff',textAlign:'center',marginTop:'50px', padding:'10px'}}>
-                    <h1>HackTheCrisis</h1>
-                    <p>Any new crisis necessitates a fresh collection of ideas and methods for coping with it. As a result, it is up to us to pool our resources and devise ideas and strategies that can be quickly developed using technology and deployed on the ground to counter the outbreak of the COVID-19 pandemic</p>
-                </Jumbotron>
-            </Fade>
-            
-            <div className='section-content'>
-            {renderCountDown()}
-            {searchBar()}
-            {selectedSection.render}
-                
-               
-
-            
-
-            </div>
-        </section>
-  
-    )
-}
+      <div className='section-content'>
+        {renderCountDown()}
+        {searchBar()}
+        {selectedSection.render}
+      </div>
+    </section>
+  );
+};
